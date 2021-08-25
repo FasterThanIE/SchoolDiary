13d411f2bb579ed913f936b3314ae5abb8873e09
FasterThanIE <tomicawarr@gmail.com>
2021-08-25 19:37:09 +0200

    Add project files.

---
+++ SchoolBook/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace SchoolBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.input_username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.input_password = new System.Windows.Forms.TextBox();
+            this.button_login = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // input_username
+            // 
+            this.input_username.Location = new System.Drawing.Point(14, 70);
+            this.input_username.Name = "input_username";
+            this.input_username.Size = new System.Drawing.Size(334, 23);
+            this.input_username.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // input_password
+            // 
+            this.input_password.Location = new System.Drawing.Point(14, 132);
+            this.input_password.Name = "input_password";
+            this.input_password.Size = new System.Drawing.Size(334, 23);
+            this.input_password.TabIndex = 3;
+            // 
+            // button_login
+            // 
+            this.button_login.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button_login.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_login.ForeColor = System.Drawing.Color.White;
+            this.button_login.Location = new System.Drawing.Point(14, 183);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(334, 37);
+            this.button_login.TabIndex = 4;
+            this.button_login.Text = "Login";
+            this.button_login.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Login into your account";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 242);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.input_password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.input_username);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox input_username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox input_password;
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
